713809886f8e8d49ffcfcf1b57b8c234c3375ac8
Zach Bateman <zkbateman@gmail.com>
2019-02-10 21:38:06 -0600

    use_multip decorator working... when used to rename func after definition (@decorator syntax impossible)

--- easy_multip/decorators.py
+++ easy_multip/decorators.py
@@ -1,27 +1,31 @@
 '''
 Module containing easy_multip decorators.
 '''
-# from .functions import maps as multip_map
 from .functions import _num_cpus
-import multiprocessing
 from multiprocessing import Process, Manager
-import functools
 from functools import wraps
-import tqdm
 import math
-from pprint import pprint as pp
 
 
 def use_multip(func, leave_one_cpu_free=True, verbose: bool=False):
     '''
     Decorator providing capability to quickly use multiprocessing with
     an expensive function operating on a list.
-    
+
     CAUTION:  To be used ONLY with functions taking a list first arg which is
     operated on by all the other (optional) args and that return another list or None.
+
+    PLEASE NOTE --> THIS DECORATOR CANNOT USE "@decorator" SYNTAX!!!
+                                 func must be pickleable for multiprocessing, and the
+                                 "@decorator" syntax prevents the func from being at the
+                                 "top-level" of the module.
+
+                                 Instead, decorate func via the following:
+
+                                    func = use_multip(_func)
+
+                                 after the _func definition (NOTE THE UNDERSCORE!!!).
     '''
-    
-    
     @wraps(func)  # allows for decorated function's docstring to come through decorator
     def inner(*args, **kwargs):
         try:
@@ -31,25 +35,20 @@ def use_multip(func, leave_one_cpu_free=True, verbose: bool=False):
         if type(first_arg) == list:
             pass
         else:
-            raise TypeError(f'The first arg of {func.__name__} must be a list when using the @use_multip decorator.') 
-        
+            raise TypeError(f'The first arg of {func.__name__} must be a list when using the @use_multip decorator.')
+
         num_cpus = _num_cpus(leave_one_cpu_free)
-        
         chunk_size = math.ceil(len(first_arg) / num_cpus)  # round UP
         sublists = [first_arg[cpu_index * chunk_size:(cpu_index + 1) * chunk_size] for cpu_index in range(num_cpus)]
-        
         with Manager() as manager:
             result_dict = manager.dict()  # dict-ish thing handling dict-like data storage among processes
-
             processes = [Process(target=_use_multip_worker,
                                  args=(func, sublists[i], i, result_dict, args[1:], kwargs))
                                     for i in range(num_cpus)]
-
             for proc in processes:
                 proc.start()
             for proc in processes:
                 proc.join()
-        
             returned = []
             for i in range(num_cpus):
                 returned.extend(result_dict[i])
@@ -58,30 +57,13 @@ def use_multip(func, leave_one_cpu_free=True, verbose: bool=False):
             return None
         else:
             return returned
-    
-    inner.__name__ = '__main__.' + inner.__name__
-    
-    
-    # functools.update_wrapper(inner, func)
     return inner
 
 
-    
 def _use_multip_worker(func, sublist, cpu_index, working_dict, *args):
     '''
     This worker function must be at the top-level of this module
     so that it can be pickled for multiprocessing.
     '''
     args, kwargs = args  # both are coming in as *args
-    # for item_dict in tqdm.tqdm(sublist):
     working_dict[cpu_index] = func(sublist, args[0], args[1], **kwargs)
-    
-        # for index, item in item_dict.items():  # there will only be one index:item pair
-            # working_dict[index] = func(item, args, kwargs)
-
-    
-# def _master_func(func, first_arg, *args, **kwargs):
-    # '''   '''
-    # return func((first_arg, *args, **kwargs))
-
-
--- tests/decorator_test.py
+++ tests/decorator_test.py
@@ -1,14 +1,15 @@
 from easy_multip.decorators import use_multip
 import time
-import random
 
 
-# first_arg = [i for i in range(20000000)]
-# first_arg = [i for i in range(10)]
 first_arg = [i for i in range(5000)]
 
 
-def test_func(list_arg, arg2, arg3, add=True):
+# decorated with "use_multip" below definition (can't use @ sugar b/c pickle issues)
+def test_func(list_arg: list, arg2, arg3, add=True):
+    '''
+    Function that takes first arg as list and returns a list or None.
+    '''
     results = []
     for x in list_arg:
         time.sleep(0.001)
@@ -17,20 +18,8 @@ def test_func(list_arg, arg2, arg3, add=True):
         else:
             results.append(x - arg2 - arg3)
     return results
+decorated_test_func = use_multip(test_func)
 
-
-@use_multip
-def test_decorator_func(list_arg, arg2, arg3, add=True):
-    results = []
-    for x in list_arg:
-        time.sleep(0.001)
-        if add:
-            results.append(x + arg2 + arg3)
-        else:
-            results.append(x - arg2 - arg3)
-    return results
-    
-# test_decorator_func2 = use_multip(test_decorator_func)
 
 
 if __name__ == '__main__':
@@ -39,12 +28,8 @@ if __name__ == '__main__':
     print(f'Normal function time: {round(time.time() - t0, 2)}')
 
     t0 = time.time()
-    results2 = test_decorator_func(first_arg, 3, 4, add=False)
-    print(f'@use_multip decorated time: {round(time.time() - t0, 2)}')
-    
-    # t0 = time.time()
-    # results2 = test_decorator_func2(first_arg, 3, 4, add=False)
-    # print(f'@use_multip decorated time: {round(time.time() - t0, 2)}')
+    results2 = decorated_test_func(first_arg, 3, 4, add=False)
+    print(f'use_multip decorated time: {round(time.time() - t0, 2)}')
 
     if results2 != results:
         print('ERROR!!! Different output from test_func and test_decorator_func!!!')
