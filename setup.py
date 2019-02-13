81800ea3d5402393813f8a1fd0738b0c99ffe291
Zach Bateman <zkbateman@gmail.com>
2019-02-12 23:45:12 -0600

    Switch to using setuptools in setup.py; version now 0.2.3

--- setup.py
+++ setup.py
@@ -1,21 +1,22 @@
-from distutils.core import setup
+import setuptools
 
 with open('README.md', 'r') as f:
     long_description = f.read()
 
-setup(name='easy_multip',
-      version='0.2.2',
-      packages=['easy_multip'],
-      license='MIT',
-      author='Zach Bateman',
-      description='Easy Python multiprocessing',
-      long_description=long_description,
-      long_description_content_type='text/markdown',
-      url='https://github.com/zachbateman/easy_multip.git',
-      download_url='https://github.com/zachbateman/easy_multip/archive/v_0.2.2.tar.gz',
-      keywords=['MULTIPROCESSING', 'SIMPLE', 'EASY', 'PARALLEL'],
-      install_requires=['tqdm'],
-      classifiers=['Development Status :: 3 - Alpha',
+setuptools.setup(
+    name='easy_multip',
+    version='0.2.3',
+    packages=['easy_multip'],
+    license='MIT',
+    author='Zach Bateman',
+    description='Easy Python multiprocessing',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/zachbateman/easy_multip.git',
+    download_url='https://github.com/zachbateman/easy_multip/archive/v_0.2.3.tar.gz',
+    keywords=['MULTIPROCESSING', 'SIMPLE', 'EASY', 'PARALLEL'],
+    install_requires=['tqdm'],
+    classifiers=['Development Status :: 3 - Alpha',
                    'License :: OSI Approved :: MIT License',
                    'Programming Language :: Python :: 3',
                    'Programming Language :: Python :: 3.6',
